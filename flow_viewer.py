9d1de3f5dd46bfe410277bf8127f9c0981286dc9
pmoat564 <paul_moat@comcast.com>
2021-05-01 16:15:33 -0400

    added search for variables to track

--- flow_viewer.py
+++ flow_viewer.py
@@ -4,7 +4,7 @@ import sys, getopt
 from xml.dom import minidom
 from sysarg_parser import parseargs
 from graph_renderer import render
-from graph_creator import make_graph
+from graph_creator import make_graph_no_BFS
 
 def print_usage():
     print('flow-viewer.py <FLOW FILE>')
@@ -17,7 +17,8 @@ def main(argv):
         print_usage()
         sys.exit(2)
     flowdom = minidom.parse(flowfile)
-    G, colors = make_graph(flowdom)
+    G, colors, track_vars = make_graph_no_BFS(flowdom)
+    print(track_vars)
     render(G, colors)
 
 if __name__ == "__main__":
--- graph_creator.py
+++ graph_creator.py
@@ -1,25 +1,32 @@
 from constants import ALL_STATES, STATE_COLORS
 import networkx as nx
 
-def make_graph(flowdom):
+def make_graph_no_BFS(flowdom):
     nodes_states_map = {}
     edges = []
+    track_vars = []
     for state in ALL_STATES:
-        scanDom(flowdom, state, edges, nodes_states_map)
+        scanDom(flowdom, state, edges, nodes_states_map, track_vars)
 
     G = nx.DiGraph()
     G.add_edges_from(edges)
-    colors = [STATE_COLORS[nodes_states_map[node]] for node in G.nodes()]
+    # colors = [STATE_COLORS[nodes_states_map[node]] for node in G.nodes()]
+    colors = ["blue"]*len(G.nodes)
 
-    return G, colors
+    return G, colors, track_vars
 
 # scans minidom node for states, and edges
-# edges appended to provided list
-#
-# returns
-#   node -> nodes found [list(str)]
-def scanDom(flowdom, stateName, edges, nodes_states_map):
+#  - edges appended to provided list
+#  - nodes_state_map[state_id] = state_type
+#  - track_vars -> list of vars that must be tracked for building graph
+def scanDom(flowdom, stateName, edges, nodes_states_map, track_vars):
     stateNodes = flowdom.getElementsByTagName(stateName)
+
+    def add_edge(state, next_state):
+        if next_state:
+            edges.append((state, next_state))
+            if "#{" in next_state:
+                track_vars.append(next_state.replace("#{", "").replace("}", ""))
 
     for stateNode in stateNodes:
         # store found state
@@ -28,13 +35,7 @@ def scanDom(flowdom, stateName, edges, nodes_states_map):
 
         # store edges
         for transitionNode in stateNode.getElementsByTagName('transition'):
-            next_state = transitionNode.getAttribute("to")
-            if next_state:
-                edges.append((state, next_state))
+            add_edge(state, transitionNode.getAttribute("to"))
         for transitionNode in stateNode.getElementsByTagName('if'):
-            next_state_then = transitionNode.getAttribute("then")
-            if next_state_then:
-                edges.append((state, next_state_then))
-            next_state_else = transitionNode.getAttribute("else")
-            if next_state_else:
-                edges.append((state, next_state_else))
+            add_edge(state, transitionNode.getAttribute("then"))
+            add_edge(state, transitionNode.getAttribute("else"))
