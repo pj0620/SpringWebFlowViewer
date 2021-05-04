dc6e0c7b5b35f8ad6c9e2508aa2cab64ee91a876
pj0620 <pjm7748@g.rit.edu>
2021-05-04 01:30:37 -0400

    simplified hashing of context

--- scope.py
+++ scope.py
@@ -1,8 +1,8 @@
 class Context:
     def __init__(self, track_vars):
         self.vars = {var: frozenset() for var in track_vars}
-        self.changed_vars = set()
         self.hash = 0
+        self.compute_hash()
 
     def contains_var(self, var_name):
         return var_name in self.vars.keys()
@@ -11,27 +11,20 @@ class Context:
         return self.vars[var_name]
 
     def set_var(self, var_name, new_vals):
-        if not new_vals:
-            raise Exception("scope variables cannot be set to empty set after initialization, messes up hashing")
-
         for i in range(len(new_vals)):
             if new_vals[i][0] == "'":
                 new_vals[i] = new_vals[i][1:]
             if new_vals[i][-1] == "'":
                 new_vals[i] = new_vals[i][:-1]
 
-        old_vals = self.vars[var_name]
         self.vars[var_name] = frozenset(new_vals)
 
         # update hash of context
-        update_changes = False
-        if var_name not in self.changed_vars:
-            update_changes = True
-            self.changed_vars.add(var_name)
-        update_changes = update_changes or old_vals != new_vals
-        if update_changes:
-            changed_set = frozenset((var_name, self.vars[var_name]) for var_name in self.changed_vars)
-            self.hash = hash(changed_set)
+        self.compute_hash()
+
+    def compute_hash(self):
+        vars_frozen = frozenset((var_name, vals) for var_name, vals in self.vars.items())
+        self.hash = hash(vars_frozen)
 
     def __hash__(self):
         return self.hash
